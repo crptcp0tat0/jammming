837119c2362851cb949c2b3a3d31cb2f7fd6e3f7
Mohammed Sahl <sahl.kably@gmail.com>
2017-11-06 18:33:19 +0400

    First Commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,83 @@
+//only playlist name clearing is left to complete
+
+import React, { Component } from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+import {Spotify} from '../../util/Spotify';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [],
+      playlistName: 'New Playlist',
+      playlistTracks: [],
+    };
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.every(savedTrack => savedTrack.id !== track.id)) {
+      tracks = this.state.playlistTracks.concat(track)
+      this.setState({playlistTracks: tracks})
+    }
+  }
+    /*tracks = tracks.find(savedTrack => savedTrack.id === track.id)
+    tracks.concat(track)
+    this.setState({playlistTracks: tracks});*/
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(savedTrack => savedTrack.id !== track.id)
+    this.setState({playlistTracks: tracks})
+  }
+
+  updatePlaylistName(name) {
+    this.setState({playlistName: name});
+  }
+
+  savePlaylist() {
+    let trackURIs = this.state.playlistTracks.map(savedTrack => savedTrack.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackURIs).then(() => {
+      this.setState({searchResults: [], playlistTracks: []});
+      this.updatePlaylistName('New Playlist');
+    })
+  }
+
+  search(searchTerm) {
+    Spotify.search(searchTerm).then(results => {this.setState({searchResults: results})})
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar onSearch={this.search}/>
+          <div className="App-playlist">
+            <SearchResults
+              searchResults={this.state.searchResults}
+              onAdd={this.addTrack}
+              isRemoval={false}/>
+            <Playlist
+              playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks}
+              onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}
+              isRemoval={true}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/util/Spotify.js
@@ -0,0 +1,164 @@
+let accessToken = null;
+let expiresIn = '';
+const clientId = 'e882f0bd7eb24cb5ba6df7e847befbdc';
+const redirectURI = 'http://boonkgangwholelottagang.surge.sh';
+//const clientSecret = 'f21823dd418a4d8f8ad67d6854c57144';
+
+export const Spotify = {
+  /*getAccessToken() {
+    expiresIn = window.location.href.match(/expires_in=([^&]+)/)[1];
+    accessToken = window.location.href.match(/access_token=([^&]+)/)[1];
+    if (accessToken) {
+      return fetch(accessToken);
+    } else if (!(accessToken)){
+      console.log('in the false section');
+
+      return fetch(`https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectURI}&scope=user-read-private%20user-read-email%20playlist-modify-public%20playlist-modify-private&response_type=token`)
+      .then(() => {
+
+        //accessToken = window.location.href.match(/access_token=([^&]*)/);
+        //expiresIn = window.location.href.match(/expires_in=([^&]*)/);
+        //window.setTimeout(() => accessToken = '', expiresIn * 1000);
+        //window.history.pushState('Access Token', null, '/');
+
+        console.log('in the double ');
+        window.location.assign(`https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`)
+        accessToken = window.location.href.match(/access_token=([^&]*)/);
+        expiresIn = window.location.href.match(/expires_in=([^&]*)/);
+        window.setTimeout(() => accessToken = '', expiresIn * 1000);
+        window.history.pushState('Access Token', null, '/');
+        console.log(accessToken);
+        return accessToken;
+      })
+    }
+  },*/
+
+  getAccessToken() {
+    if (accessToken) {
+      return fetch(accessToken);
+    } else if (window.location.href.match(/access_token=([^&]*)/)){
+      accessToken = window.location.href.match(/access_token=([^&]*)/)[1];
+      expiresIn = window.location.href.match(/expires_in=([^&]*)/)[1];
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+      return fetch(accessToken);
+    } else {
+      window.location = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`
+    }
+  },
+
+  search(searchTerm) {
+    return Spotify.getAccessToken()
+    .then(() => {
+      return fetch(`https://cors-anywhere.herokuapp.com/https://api.spotify.com/v1/search?type=track&q=${searchTerm}`, {
+        method:'GET',
+        headers: {Authorization: `Bearer ${accessToken}`}
+      })
+    })
+    .then(response => {
+      return response.json()
+    })
+    .then(jsonResponse => {
+      if (jsonResponse.tracks) {
+        return jsonResponse.tracks.items.map(track => ({
+          id: track.id,
+          Name: track.name,
+          Artist: track.artists[0].name,
+          Album: track.album.name,
+          uri: track.uri
+        }))
+      } else {
+        return ([])
+      }
+    })
+  },
+
+  savePlaylist(name, trackURIs) {
+    let userID = '';
+    if((name === '') || (trackURIs === [])) {
+      return;
+    };
+
+    return Spotify.getAccessToken()
+    .then(() => fetch('https://cors-anywhere.herokuapp.com/https://api.spotify.com/v1/me', {headers: {Authorization: `Bearer ${accessToken}`}}))
+    .then(response => {
+      return response.json()
+    })
+    .then(jsonResponse => {
+      userID = jsonResponse.id
+      return jsonResponse.id
+    })
+    .then(userID => {
+      return fetch(`https://cors-anywhere.herokuapp.com/https://api.spotify.com/v1/users/${userID}/playlists`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({name: name})
+      })
+    })
+    .then(response => {
+      return response.json()
+    })
+    .then(jsonResponse => {
+      return jsonResponse.id
+    })
+    .then(playlistID => {
+      return fetch(`https://cors-anywhere.herokuapp.com/https://api.spotify.com/v1/users/${userID}/playlists/${playlistID}/tracks`,{
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({uris: trackURIs})
+      })
+    });
+  }
+};
+
+//export default Spotify;
+/*let playlistID = Spotify.getAccessToken()
+.then(fetch(`https://cors-anywhere.herokuapp.com/https://api.spotify.com/v1/users/${userID}/playlists/${playlistID}/tracks`, {
+headers: {
+Authorization: `Bearer ${accessToken}`,
+'Content-Type': 'application/json'
+},
+method: 'POST',
+body: JSON.stringify({
+uris: trackURIs
+})
+}))
+.then(response => {
+return response.json()
+})
+.then(jsonResponse => {
+return jsonResponse.id
+});*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*else if ((window.location.href.match(/access_token=([^&]*)/)) && (window.location.href.match(/expires_in=([^&]*)/))) {
+accessToken = window.location.href.match(/access_token=([^&]*)/);
+const expiresIn = window.location.href.match(/expires_in=([^&]*)/);
+window.setTimeout(() => accessToken = '', expiresIn * 1000);
+window.history.pushState('Access Token', null, '/');
+} else {
+let URLString = fetch(`https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`),{
+
+}
+}*/
